1248917e09d6f869ade51d062a40f5906589f257
Slava Savitskiy <slava@sinch.com>
2014-12-10 09:16:30 +0100

    Updated pip package to be a one-file module.

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ def readme():
 
 setup(
   name = 'sinchsms',
-  version = '1.0',
+  version = '1.0.3',
   description = 'A module to send sms using the Sinch REST apis, www.sinch.com',
   long_description = readme(),
   author = 'Slava Savitskiy',
   author_email = 'slava@sinch.com',
   url = 'https://github.com/sinch/python-sinch-sms.git',
   keywords = ['sms', 'sinch'],
+  py_modules = ['sinchsms'],
   classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
